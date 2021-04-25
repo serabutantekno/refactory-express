bdf6aaa871f980e478c902641c1a4a9a9dc80d9d
serabutantekno <madyaneka@protonmail.com>
2021-04-26 06:46:40 +0700

    catching data from request

--- app.js
+++ app.js
@@ -3,12 +3,22 @@ const app = express()
 const port = 3000
 
 
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+
 app.get("/", (req, res) => {
+    console.log(req.query)
     res.send("get")
 })
 
 
 app.post("/", (req, res) => {
+    /* Calling:
+            - req.body (contains json) requires the use of json parser ==> express.json()
+            - req.body (contains form data) requires the use of ==> express.urlencoded()
+    */
+    console.log(req.body)
     res.send("post")
 })
 
